de10a5545f7f6176ab6c9729692f018c8bcb7262
Franco Paganoni <114701161+Fpaganoni@users.noreply.github.com>
2024-10-24 17:54:43 +0200

    PX Layouts done, next step is to develop one animation to the hamburguer menu and modifiy titles and paragraphs with the images

--- room home page master/hamburguer.js
+++ room home page master/hamburguer.js
@@ -1,6 +1,7 @@
-let menuHamburguer = document.querySelector(".nav_hamburguer");
-let main = document.querySelector(".main");
-let iconClose = document.querySelector(".icon_close");
+let menuHamburguer = document.querySelector(".nav_hamburguer"),
+  main = document.querySelector(".main"),
+  iconClose = document.querySelector(".icon_close"),
+  main_nav = document.querySelector(".main_nav");
 
 menuHamburguer.addEventListener("click", () => {
   main.classList.toggle("open_menu");
