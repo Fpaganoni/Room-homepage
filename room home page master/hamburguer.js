2b1dedb7e83d80e4db55e5cc9e1811475d86f5fc
Franco Paganoni <114701161+Fpaganoni@users.noreply.github.com>
2024-10-30 19:24:42 +0100

    Burguer menu width some details, transitions and others

--- room home page master/hamburguer.js
+++ room home page master/hamburguer.js
@@ -1,28 +1,18 @@
 let menuHamburguer = document.querySelector(".nav_hamburguer"),
   main = document.querySelector(".main"),
   iconClose = document.querySelector(".icon_close"),
-  main_nav = document.querySelector(".main_nav"),
   nav_links = document.querySelector(".nav_links");
 
 // Function to reset animations
 
-let resetAnimation = (el) => {
-  el.classList.remove("open_links", "close_links");
-  return el.offsetWidth;
-};
-
 // Listeners to open & close de menu
 
 menuHamburguer.addEventListener("click", () => {
-  resetAnimation(nav_links);
   nav_links.classList.add("open_links");
-  nav_links.classList.remove(".close_links");
   main.classList.add("open_menu");
 });
 
 iconClose.addEventListener("click", () => {
-  resetAnimation(nav_links);
   nav_links.classList.remove("open_links");
-  nav_links.classList.add(".close_links");
   main.classList.remove("open_menu");
 });
