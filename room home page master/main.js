298f3009d4c7b5e17f31becb41a534bd0b433f94
Franco Paganoni <114701161+Fpaganoni@users.noreply.github.com>
2024-11-05 12:46:01 +0100

    project almost finished, wait for details

--- room home page master/main.js
+++ room home page master/main.js
@@ -1,41 +1,43 @@
 // SLIDER
 
 /* Variables to call */
-let backgroundsMobile = document.querySelector(".backgrounds-mobile");
-let backgroundsDesktop = document.querySelector(".backgrounds-desktop");
-let controlRight = document.querySelector(".main_controls_right");
-let controlLeft = document.querySelector(".main_controls_left");
+let backgroundsMobile = document.querySelector(".backgrounds-mobile"),
+  backgroundsDesktop = document.querySelector(".backgrounds-desktop"),
+  controlRight = document.querySelector(".main_controls_right"),
+  controlLeft = document.querySelector(".main_controls_left"),
+  containerContent = document.querySelector(".container_contents");
 
 let operacion = 0;
 
 /* Functions for arrows */
 
-let moveToRight = () => {
+let moveToRightImg = () => {
   operacion = operacion + 100;
   if (operacion > 200) {
     operacion = 0;
   }
 
+  containerContent.style.transform = `translate(-${operacion}%)`;
   backgroundsMobile.style.transform = `translate(-${operacion}%)`;
   backgroundsDesktop.style.transform = `translate(-${operacion}%)`;
 };
 
-let moveToLeft = () => {
+let moveToLeftImg = () => {
   operacion = operacion - 100;
   if (operacion < 0) {
     operacion = 200;
   }
 
+  containerContent.style.transform = `translate(-${operacion}%)`;
   backgroundsMobile.style.transform = `translate(-${operacion}%)`;
   backgroundsDesktop.style.transform = `translate(-${operacion}%)`;
 };
 
 /* Listeners for arrows */
 controlRight.addEventListener("click", (e) => {
-  moveToRight();
+  moveToRightImg();
 });
 
 controlLeft.addEventListener("click", (e) => {
-  moveToLeft();
-  console.log("click");
+  moveToLeftImg();
 });
